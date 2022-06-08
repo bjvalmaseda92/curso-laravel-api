7b7f80daef0b7acaf1e4fab9ea298d11369ffb04
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-08 12:25:45 -0400

    feat: ✨ add files to start to build api v2

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,20 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\PostController as PostControllerV1;
+use App\Http\Controllers\Api\V2\PostController as PostControllerV2;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource("v1/posts", PostController::class)->only([
+//Route V2
+Route::apiResource("v1/posts", PostControllerV1::class)->only([
     "index",
     "show",
     "destroy",
+]);
+
+//Route V2
+Route::apiResource("v2/posts", PostControllerV2::class)->only([
+    "index",
+    "show",
 ]);
